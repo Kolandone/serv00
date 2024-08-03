56664884d497e86c75d5b96d1f944b0e4a900e4f
Kolandone <sjamshid81@gmail.com>
2024-08-03 18:32:14 +0330

    Create KOLANDserv00.sh

---
+++ KOLANDserv00.sh
@@ -0,0 +1,485 @@
+#!/bin/bash
+
+# 定义颜色
+re="\033[0m"
+red="\033[1;91m"
+green="\e[1;32m"
+yellow="\e[1;33m"
+purple="\e[1;35m"
+red() { echo -e "\e[1;91m$1\033[0m"; }
+green() { echo -e "\e[1;32m$1\033[0m"; }
+yellow() { echo -e "\e[1;33m$1\033[0m"; }
+purple() { echo -e "\e[1;35m$1\033[0m"; }
+reading() { read -p "$(red "$1")" "$2"; }
+
+USERNAME=$(whoami)
+HOSTNAME=$(hostname)
+export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export NEZHA_SERVER=${NEZHA_SERVER:-''} 
+export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
+export NEZHA_KEY=${NEZHA_KEY:-''} 
+export ARGO_DOMAIN=${ARGO_DOMAIN:-''}   
+export ARGO_AUTH=${ARGO_AUTH:-''} 
+
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
+[ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR")
+
+read_vmess_port() {
+    while true; do
+        reading "Please enter the vmess port (tcp port open on the panel): " vmess_port
+        if [[ "$vmess_port" =~ ^[0-9]+$ ]] && [ "$vmess_port" -ge 1 ] && [ "$vmess_port" -le 65535 ]; then
+            green "Your vmess port is: $vmess_port"
+            break
+        else
+            yellow "Input error, please re-enter the TCP port opened by the panel"
+        fi
+    done
+}
+
+read_hy2_port() {
+    while true; do
+        reading "Please enter the hysteria2 port (the UDP port opened by the panel): " hy2_port
+        if [[ "$hy2_port" =~ ^[0-9]+$ ]] && [ "$hy2_port" -ge 1 ] && [ "$hy2_port" -le 65535 ]; then
+            green "Your hysteria2 port is: $hy2_port"
+            break
+        else
+            yellow "Input error, please re-enter the UDP port opened by the panel"
+        fi
+    done
+}
+
+read_tuic_port() {
+    while true; do
+        reading "Please enter the Tuic port (the UDP port opened by the panel): " tuic_port
+        if [[ "$tuic_port" =~ ^[0-9]+$ ]] && [ "$tuic_port" -ge 1 ] && [ "$tuic_port" -le 65535 ]; then
+            green "Your tuic port is: $tuic_port"
+            break
+        else
+            yellow "Input error, please re-enter the UDP port opened by the panel"
+        fi
+    done
+}
+
+read_nz_variables() {
+  if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+      green "Use the custom variable Nezha to run the Nezha probe"
+      return
+  else
+      reading "Do I need to install the Nezha probe?？【y/n】: " nz_choice
+      [[ -z $nz_choice ]] && return
+      [[ "$nz_choice" != "y" && "$nz_choice" != "Y" ]] && return
+      reading "Please enter the Nezha probe domain name or IP：" NEZHA_SERVER
+      green "Your Nezha domain name is: $NEZHA_SERVER"
+      reading "Please enter the Nezha probe port (press Enter to skip and use 5555 by default):" NEZHA_PORT
+      [[ -z $NEZHA_PORT ]] && NEZHA_PORT="5555"
+      green "Your Nezha port is: $NEZHA_PORT"
+      reading "Please enter the Nezha probe key:" NEZHA_KEY
+      green "Your Nezha key is: $NEZHA_KEY"
+  fi
+}
+
+install_singbox() {
+echo -e "${yellow}This script coexists with four protocols at the same time${purple}(vmess-ws,vmess-ws-tls(argo),hysteria2,tuic)${re}"
+echo -e "${yellow}Before starting the run, make sure that the panel${purple}3 ports have been opened, one tcp port and two udp ports${re}"
+echo -e "${yellow}panel${purple}Additional services middle Run your own applications${yellow}has been turned on as${purplw}Enabled${yellow}state${re}"
+reading "\nAre you sure to continue the installation?？【y/n】: " choice
+  case "$choice" in
+    [Yy])
+        cd $WORKDIR
+        read_nz_variables
+        read_vmess_port
+        read_hy2_port
+        read_tuic_port
+        argo_configure
+        generate_config
+        download_singbox && wait
+        run_sb && sleep 3
+        get_links
+      ;;
+    [Nn]) exit 0 ;;
+    *) red "Invalid selection, please enter y or n" && menu ;;
+  esac
+}
+
+uninstall_singbox() {
+  reading "\nAre you sure you want to uninstall?？【y/n】: " choice
+    case "$choice" in
+       [Yy])
+          kill -9 $(ps aux | grep '[w]eb' | awk '{print $2}')
+          kill -9 $(ps aux | grep '[b]ot' | awk '{print $2}')
+          kill -9 $(ps aux | grep '[n]pm' | awk '{print $2}')
+          rm -rf $WORKDIR
+          ;;
+        [Nn]) exit 0 ;;
+    	*) red "Invalid selection, please enter y or n" && menu ;;
+    esac
+}
+
+argo_configure() {
+  if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
+      reading "Do you need to use fixed argo tunnels?？【y/n】: " argo_choice
+      [[ -z $argo_choice ]] && return
+      [[ "$argo_choice" != "y" && "$argo_choice" != "Y" && "$argo_choice" != "n" && "$argo_choice" != "N" ]] && { red "Invalid selection, please enter y or n"; return; }
+      if [[ "$argo_choice" == "y" || "$argo_choice" == "Y" ]]; then
+          reading "Please enter the argo fixed tunnel domain name: " ARGO_DOMAIN
+          green "Your argo fixed tunnel domain name is: $ARGO_DOMAIN"
+          reading "Please enter the argo fixed tunnel key（Json or Token）: " ARGO_AUTH
+          green "Your argo fixed tunnel key is: $ARGO_AUTH"
+	  echo -e "${red}Notice：${purple}To use token, you need to set the tunnel port in the cloudflare background to be consistent with the TCP port opened by the panel.${re}"
+      else
+          green "ARGO tunnel variable not set, temporary tunnel will be used"
+          return
+      fi
+  fi
+
+  if [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+    echo $ARGO_AUTH > tunnel.json
+    cat > tunnel.yml << EOF
+tunnel: $(cut -d\" -f12 <<< "$ARGO_AUTH")
+credentials-file: tunnel.json
+protocol: http2
+
+ingress:
+  - hostname: $ARGO_DOMAIN
+    service: http://localhost:$vmess_port
+    originRequest:
+      noTLSVerify: true
+  - service: http_status:404
+EOF
+  else
+    green "ARGO_AUTH mismatch TunnelSecret,use token connect to tunnel"
+  fi
+}
+
+# Download Dependency Files
+download_singbox() {
+  ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
+  if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
+      FILE_INFO=("https://github.com/eooce/test/releases/download/arm64/sb web" "https://github.com/eooce/test/releases/download/arm64/bot13 bot" "https://github.com/eooce/test/releases/download/ARM/swith npm")
+  elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
+      FILE_INFO=("https://eooce.2go.us.kg/web web" "https://eooce.2go.us.kg/bot bot" "https://eooce.2go.us.kg/npm npm")
+  else
+      echo "Unsupported architecture: $ARCH"
+      exit 1
+  fi
+  for entry in "${FILE_INFO[@]}"; do
+      URL=$(echo "$entry" | cut -d ' ' -f 1)
+      NEW_FILENAME=$(echo "$entry" | cut -d ' ' -f 2)
+      FILENAME="$DOWNLOAD_DIR/$NEW_FILENAME"
+      if [ -e "$FILENAME" ]; then
+          green "$FILENAME already exists, Skipping download"
+      else
+          wget -q -O "$FILENAME" "$URL"
+          green "Downloading $FILENAME"
+      fi
+      chmod +x $FILENAME
+  done
+}
+
+# Generating Configuration Files
+generate_config() {
+
+    openssl ecparam -genkey -name prime256v1 -out "private.key"
+    openssl req -new -x509 -days 3650 -key "private.key" -out "cert.pem" -subj "/CN=$USERNAME.serv00.net"
+
+  cat > config.json << EOF
+{
+  "log": {
+    "disabled": true,
+    "level": "info",
+    "timestamp": true
+  },
+  "dns": {
+    "servers": [
+      {
+        "tag": "google",
+        "address": "tls://8.8.8.8",
+        "strategy": "ipv4_only",
+        "detour": "direct"
+      }
+    ],
+    "rules": [
+      {
+        "rule_set": [
+          "geosite-openai"
+        ],
+        "server": "wireguard"
+      },
+      {
+        "rule_set": [
+          "geosite-netflix"
+        ],
+        "server": "wireguard"
+      },
+      {
+        "rule_set": [
+          "geosite-category-ads-all"
+        ],
+        "server": "block"
+      }
+    ],
+    "final": "google",
+    "strategy": "",
+    "disable_cache": false,
+    "disable_expire": false
+  },
+    "inbounds": [
+    {
+       "tag": "hysteria-in",
+       "type": "hysteria2",
+       "listen": "::",
+       "listen_port": $hy2_port,
+       "users": [
+         {
+             "password": "$UUID"
+         }
+     ],
+     "masquerade": "https://bing.com",
+     "tls": {
+         "enabled": true,
+         "alpn": [
+             "h3"
+         ],
+         "certificate_path": "cert.pem",
+         "key_path": "private.key"
+        }
+    },
+    {
+      "tag": "vmess-ws-in",
+      "type": "vmess",
+      "listen": "::",
+      "listen_port": $vmess_port,
+      "users": [
+      {
+        "uuid": "$UUID"
+      }
+    ],
+    "transport": {
+      "type": "ws",
+      "path": "/vmess",
+      "early_data_header_name": "Sec-WebSocket-Protocol"
+      }
+    },
+    {
+      "tag": "tuic-in",
+      "type": "tuic",
+      "listen": "::",
+      "listen_port": $tuic_port,
+      "users": [
+        {
+          "uuid": "$UUID",
+          "password": "admin123"
+        }
+      ],
+      "congestion_control": "bbr",
+      "tls": {
+        "enabled": true,
+        "alpn": [
+          "h3"
+        ],
+        "certificate_path": "cert.pem",
+        "key_path": "private.key"
+      }
+    }
+
+ ],
+    "outbounds": [
+    {
+      "type": "direct",
+      "tag": "direct"
+    },
+    {
+      "type": "block",
+      "tag": "block"
+    },
+    {
+      "type": "dns",
+      "tag": "dns-out"
+    },
+    {
+      "type": "wireguard",
+      "tag": "wireguard-out",
+      "server": "162.159.195.100",
+      "server_port": 4500,
+      "local_address": [
+        "172.16.0.2/32",
+        "2606:4700:110:83c7:b31f:5858:b3a8:c6b1/128"
+      ],
+      "private_key": "mPZo+V9qlrMGCZ7+E6z2NI6NOV34PD++TpAR09PtCWI=",
+      "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+      "reserved": [
+        26,
+        21,
+        228
+      ]
+    }
+  ],
+  "route": {
+    "rules": [
+      {
+        "protocol": "dns",
+        "outbound": "dns-out"
+      },
+      {
+        "ip_is_private": true,
+        "outbound": "direct"
+      },
+      {
+        "rule_set": [
+          "geosite-openai"
+        ],
+        "outbound": "wireguard-out"
+      },
+      {
+        "rule_set": [
+          "geosite-netflix"
+        ],
+        "outbound": "wireguard-out"
+      },
+      {
+        "rule_set": [
+          "geosite-category-ads-all"
+        ],
+        "outbound": "block"
+      }
+    ],
+    "rule_set": [
+      {
+        "tag": "geosite-netflix",
+        "type": "remote",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-netflix.srs",
+        "download_detour": "direct"
+      },
+      {
+        "tag": "geosite-openai",
+        "type": "remote",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/sing/geo/geosite/openai.srs",
+        "download_detour": "direct"
+      },      
+      {
+        "tag": "geosite-category-ads-all",
+        "type": "remote",
+        "format": "binary",
+        "url": "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-category-ads-all.srs",
+        "download_detour": "direct"
+      }
+    ],
+    "final": "direct"
+   },
+   "experimental": {
+      "cache_file": {
+      "path": "cache.db",
+      "cache_id": "mycacheid",
+      "store_fakeip": true
+    }
+  }
+}
+EOF
+}
+
+# running files
+run_sb() {
+  if [ -e npm ]; then
+    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
+    fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+        export TMPDIR=$(pwd)
+        nohup ./npm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+	sleep 2
+        pgrep -x "npm" > /dev/null && green "npm is running" || { red "npm is not running, restarting..."; pkill -x "npm" && nohup ./npm -s "${NEZHA_SERVER}:${NEZHA_PORT}" -p "${NEZHA_KEY}" ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; purple "npm restarted"; }
+    else
+        purple "NEZHA variable is empty,skiping runing"
+    fi
+  fi
+
+  if [ -e web ]; then
+    nohup ./web run -c config.json >/dev/null 2>&1 &
+    sleep 2
+    pgrep -x "web" > /dev/null && green "web is running" || { red "web is not running, restarting..."; pkill -x "web" && nohup ./web run -c config.json >/dev/null 2>&1 & sleep 2; purple "web restarted"; }
+  fi
+
+  if [ -e bot ]; then
+    if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token ${ARGO_AUTH}"
+    elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+      args="tunnel --edge-ip-version auto --config tunnel.yml run"
+    else
+      args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile boot.log --loglevel info --url http://localhost:$vmess_port"
+    fi
+    nohup ./bot $args >/dev/null 2>&1 &
+    sleep 2
+    pgrep -x "bot" > /dev/null && green "bot is running" || { red "bot is not running, restarting..."; pkill -x "bot" && nohup ./bot "${args}" >/dev/null 2>&1 & sleep 2; purple "bot restarted"; }
+  fi
+ 
+}
+
+get_links(){
+  get_argodomain() {
+    if [[ -n $ARGO_AUTH ]]; then
+      echo "$ARGO_DOMAIN"
+    else
+      grep -oE 'https://[[:alnum:]+\.-]+\.trycloudflare\.com' boot.log | sed 's@https://@@'
+    fi
+  }
+argodomain=$(get_argodomain)
+echo -e "\e[1;32mArgoDomain:\e[1;35m${argodomain}\e[0m\n"
+sleep 1
+# get ip
+IP=$(curl -s ipv4.ip.sb || { ipv6=$(curl -s --max-time 1 ipv6.ip.sb); echo "[$ipv6]"; })
+sleep 1
+# get ipinfo
+ISP=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18}' | sed -e 's/ /_/g') 
+sleep 1
+yellow "Note: The skip certificate verification of v2ray or other software needs to be set to true, otherwise the hy2 or tuic node may be blocked.\n"
+cat > list.txt <<EOF
+vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$ISP\", \"add\": \"$IP\", \"port\": \"$vmess_port\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\", \"path\": \"/vmess?ed=2048\", \"tls\": \"\", \"sni\": \"\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)
+
+vmess://$(echo "{ \"v\": \"2\", \"ps\": \"$ISP\", \"add\": \"www.visa.com.tw\", \"port\": \"443\", \"id\": \"$UUID\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"$argodomain\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"$argodomain\", \"alpn\": \"\", \"fp\": \"\"}" | base64 -w0)
+
+hysteria2://$UUID@$IP:$hy2_port/?sni=www.bing.com&alpn=h3&insecure=1#$ISP
+
+tuic://$UUID:admin123@$IP:$tuic_port?sni=www.bing.com&congestion_control=bbr&udp_relay_mode=native&alpn=h3&allow_insecure=1#$ISP
+EOF
+cat list.txt
+purple "list.txt saved successfully"
+purple "Running done!"
+sleep 3 
+rm -rf web bot npm boot.log config.json sb.log core tunnel.yml tunnel.json
+
+}
+
+#main menu
+menu() {
+   clear
+   echo ""
+   purple "=== Serv00|ct8Laowang sing-box one-click installation script ===\n"
+   echo -e "${green}Script address：${re}${yellow}https://github.com/eooce/Sing-box${re}\n"
+   echo -e "${green}Feedback Forum：${re}${yellow}https://bbs.vps8.me${re}\n"
+   echo -e "${green}TGfeedback group：${re}${yellow}https://t.me/vps888${re}\n"
+   echo -e "${green}YOUTUBE CHANNEL：${re}${yellow}https://www.youtube.com/@kolandone${re}\n"
+   purple "Please reprint from a well-known source and please do not abuse it.\n"
+   green "1. Install sing-box"
+   echo  "==============="
+   red "2. Uninstall sing-box"
+   echo  "==============="
+   green "3. View node information"
+   echo  "==============="
+   red "0. Exit script"
+   echo "==========="
+   reading "Please enter your choice(0-3): " choice
+   echo ""
+    case "${choice}" in
+        1) install_singbox ;;
+        2) uninstall_singbox ;; 
+        3) cat $WORKDIR/list.txt ;; 
+        0) exit 0 ;;
+        *) red "Invalid option, please enter 0 to 3" ;;
+    esac
+}
+menu
